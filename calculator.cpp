b5ce297de2fc6c035f10f275f953e52aabbd5806
varun <sricharanc64@gmail.com>
2025-04-14 23:38:33 +0530

    calculator added

---
+++ calculator.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// Feature: Addition
+double add(double a, double b) {
+    return a + b;
+}
+
+
+int main() {
+    double num1, num2;
+    char op;
+
+    cout << "Welcome to the Calculator!\n";
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch(op) {
+        case '+':
+            cout << "Result: " << add(num1, num2) << endl;
+            break;
+        default:
+            cout << "Invalid operator!" << endl;
+    }
+
+    return 0;
+}
