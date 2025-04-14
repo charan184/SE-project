392c74fcb68c486222e2e176658132902dd66fe3
varun <sricharanc64@gmail.com>
2025-04-14 23:41:32 +0530

    subtraction included

--- calculator.cpp
+++ calculator.cpp
@@ -6,6 +6,10 @@ double add(double a, double b) {
     return a + b;
 }
 
+double subtract(double a, double b) {
+    return a - b;
+}
+
 
 int main() {
     double num1, num2;
@@ -21,6 +25,9 @@ int main() {
     switch(op) {
         case '+':
             cout << "Result: " << add(num1, num2) << endl;
+            break;
+        case '-':
+            cout << "Result: " << subtract(num1, num2) << endl;
             break;
         default:
             cout << "Invalid operator!" << endl;
